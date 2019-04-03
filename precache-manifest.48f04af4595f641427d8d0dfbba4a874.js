1966e73f20e4deb4f433d663ca0583d4d98200d0
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2019-04-03 14:23:23 +0530

    Updates

---
+++ precache-manifest.48f04af4595f641427d8d0dfbba4a874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35bfd40eac1f596b7450",
+    "url": "/phonebook-directory/static/js/runtime~main.5c1ba39c.js"
+  },
+  {
+    "revision": "daa9d4461126072817d3",
+    "url": "/phonebook-directory/static/js/main.9b5ececd.chunk.js"
+  },
+  {
+    "revision": "e06e3b5f755590c1e611",
+    "url": "/phonebook-directory/static/js/2.8534d51a.chunk.js"
+  },
+  {
+    "revision": "daa9d4461126072817d3",
+    "url": "/phonebook-directory/static/css/main.2a48a22f.chunk.css"
+  },
+  {
+    "revision": "e06e3b5f755590c1e611",
+    "url": "/phonebook-directory/static/css/2.40c9676c.chunk.css"
+  },
+  {
+    "revision": "7f9abac6c420e80ad7723d7fddae5e69",
+    "url": "/phonebook-directory/index.html"
+  }
+];
